47ff303c8573bad3216c9077c50b74d2336a726f
Tobrun <tobrun.van.nuland@gmail.com>
2018-01-26 15:32:16 +0100

    [android] - translucent surface on TextureView

--- platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/textureview/TextureViewResizeActivity.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/testapp/activity/textureview/TextureViewResizeActivity.java
@@ -3,16 +3,20 @@ package com.mapbox.mapboxsdk.testapp.activity.textureview;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.ActionBar;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.style.layers.Layer;
 import com.mapbox.mapboxsdk.testapp.R;
 
+import static com.mapbox.mapboxsdk.style.layers.Property.NONE;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.visibility;
+
 /**
- * Test resizing a {@link android.view.TextureView} backed map on the fly.
+ * Test for resizing a {@link android.view.TextureView} and using the translucent surface configuration
  */
 public class TextureViewResizeActivity extends AppCompatActivity {
 
@@ -23,22 +27,32 @@ public class TextureViewResizeActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_textureview_resize);
-    setupToolbar();
     setupMapView(savedInstanceState);
     setupFab();
   }
 
-  private void setupToolbar() {
-    ActionBar actionBar = getSupportActionBar();
-    if (actionBar != null) {
-      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-      getSupportActionBar().setHomeButtonEnabled(true);
-    }
-  }
-
   private void setupMapView(Bundle savedInstanceState) {
     mapView = (MapView) findViewById(R.id.mapView);
-    mapView.getMapAsync(mapboxMap -> TextureViewResizeActivity.this.mapboxMap = mapboxMap);
+    mapView.onCreate(savedInstanceState);
+
+    // provide a background to showcase the translucent configuration
+    mapView.setBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.blueAccent, getTheme()));
+
+    mapView.getMapAsync(map -> {
+      mapboxMap = map;
+
+      // hide layers obstructing the mapview background
+      hideLayer("background");
+      hideLayer("water");
+      hideLayer("water-shadow");
+    });
+  }
+
+  private void hideLayer(String layerId) {
+    Layer layer = mapboxMap.getLayer(layerId);
+    if (layer != null) {
+      layer.setProperties(visibility(NONE));
+    }
   }
 
   private void setupFab() {
